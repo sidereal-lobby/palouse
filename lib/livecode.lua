7977e5c9c1e10e4d2f33279cc5a46d3c5be0dac6
Ryan Laws <ryanlaws@gmail.com>
2021-10-27 06:44:40 -0700

    Move libs around
    - Rename dawn.lua -> fx.lua (that's all that was in there)
    - Return private table in livecode
    - Explicitly expose livecode to p in includes (equivalent)

--- lib/livecode.lua
+++ lib/livecode.lua
@@ -1,14 +1,22 @@
-p                  = {}        -- protect the palouse
-p.ape              = s{1}      -- ape
-p.root             = s{60}     -- root
-p.tempo            = s{120}    -- tempo
-p.reverb           = s{1}      -- off 1, on 2
-p.rev_return_level = s{0.0}    -- db
-p.rev_pre_delay    = s{60.0}   -- ms
-p.rev_lf_fc        = s{200.0}  -- hz
-p.rev_low_time     = s{6.0}    -- seconds
-p.rev_mid_time     = s{6.0}    -- seconds
-p.rev_hf_damping   = s{6000.0} -- hz
-p.delay_beats      = s{3/4}    -- beats
-p.delay_decay      = s{5}      -- seconds
-p.delay_lag        = s{0.05}   -- seconds
+local livecode            = {}        -- protect the palouse
+
+-- Music
+livecode.root             = s{60}     -- root
+livecode.tempo            = s{120}    -- tempo
+
+-- ?
+livecode.ape              = s{1}      -- ape
+
+-- fx
+livecode.reverb           = s{1}      -- off 1, on 2
+livecode.rev_return_level = s{0.0}    -- db
+livecode.rev_pre_delay    = s{60.0}   -- ms
+livecode.rev_lf_fc        = s{200.0}  -- hz
+livecode.rev_low_time     = s{6.0}    -- seconds
+livecode.rev_mid_time     = s{6.0}    -- seconds
+livecode.rev_hf_damping   = s{6000.0} -- hz
+livecode.delay_beats      = s{3/4}    -- beats
+livecode.delay_decay      = s{5}      -- seconds
+livecode.delay_lag        = s{0.05}   -- seconds
+
+return livecode
